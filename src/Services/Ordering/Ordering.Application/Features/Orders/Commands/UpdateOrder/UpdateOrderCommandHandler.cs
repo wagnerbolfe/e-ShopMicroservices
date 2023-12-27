9be65eab9479035862d42af2d8cedeee62c96103
Wagner Bolfe <shaper@live.com>
2023-12-26 22:45:50 -0300

    Last Updates

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Features.Orders.Commands.UpdateOrder;
@@ -27,7 +28,7 @@ public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
         if (orderToUpdate == null)
         {
             _logger.LogError("Order not exist on database");
-            throw new NullReferenceException();
+            throw new NotFoundException(nameof(Order), request.Id);
         }
 
         _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
